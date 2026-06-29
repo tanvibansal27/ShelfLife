bf8bc2a951969d89d146d1ef4f49ea26faf76bec
Tanvi bansal <tanvibansal099@gmail.com>
2026-06-29 15:45:31 +0530

    Fix Render port

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("ShelfLife Backend Running 🚀");
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
